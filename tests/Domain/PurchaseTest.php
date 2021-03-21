80c8340f4c05241d8466d5a079040909631928a8
Mirosław Lasek <miroslaw.lasek@drophub.pl>
2021-03-21 16:46:50 +0100

    Add unit test for Purchase
    -purchase product
    -purchase multiple products
    -purchase when product already purchased throw exception
    -purchase when customer is not legal age throw exception
    -purchase when no products added throw exception

---
+++ tests/Domain/PurchaseTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Domain;
+
+use App\Domain\Customer;
+use App\Domain\Product;
+use App\Domain\Purchase;
+use App\Exception\CustomerNotAllowedToPurchase;
+use App\Exception\NoProductAddedToPurchase;
+use App\Exception\ProductAlreadyPurchased;
+use Money\Money;
+use PHPUnit\Framework\TestCase;
+
+class PurchaseTest extends TestCase
+{
+    public function testCustomerCanPurchaseProduct(): void
+    {
+        $customer = new Customer(uniqid('c', true), 18);
+
+        $product = new Product(uniqid('p', true), Money::PLN(random_int(1, 500)));
+
+        $purchaseOrder = new Purchase(uniqid('pr', true), $customer);
+
+        $purchaseOrder->addProduct($product);
+        $purchaseOrder->confirm();
+
+        $this->assertTrue($purchaseOrder->isConfirmed());
+    }
+
+    public function testCustomerCanPurchaseMultipleProducts(): void
+    {
+        $customer = new Customer(uniqid('c', true), 18);
+
+        $product = new Product(uniqid('p', true), Money::PLN(random_int(1, 500)));
+        $product2 = new Product(uniqid('p', true), Money::PLN(random_int(1, 500)));
+
+        $purchaseOrder = new Purchase(uniqid('pr', true), $customer);
+
+        $purchaseOrder->addProduct($product);
+        $purchaseOrder->addProduct($product2);
+        $purchaseOrder->confirm();
+
+        $this->assertTrue($purchaseOrder->isConfirmed());
+    }
+
+    public function testCustomerCannotPurchaseWhenIsNotLegalAge(): void
+    {
+        $this->expectException(CustomerNotAllowedToPurchase::class);
+
+        $customer = new Customer(uniqid('c', true), 16);
+
+        $product = new Product(uniqid('p', true), Money::PLN(random_int(1, 500)));
+
+        $purchaseOrder = new Purchase(uniqid('pr', true), $customer);
+
+        $purchaseOrder->addProduct($product);
+        $purchaseOrder->confirm();
+    }
+
+    public function testCustomerCannotPurchaseWhenProductIsAlreadyPurchased(): void
+    {
+        $this->expectException(ProductAlreadyPurchased::class);
+
+        $customer = new Customer(uniqid('c', true), 18);
+
+        $product = new Product(uniqid('p', true), Money::PLN(random_int(1, 500)));
+
+        $purchaseOrder = new Purchase(uniqid('pr', true), $customer);
+        $purchaseOrder2 = new Purchase(uniqid('pr', true), $customer);
+
+        $purchaseOrder->addProduct($product);
+        $purchaseOrder->confirm();
+
+        $purchaseOrder2->addProduct($product);
+        $purchaseOrder2->confirm();
+    }
+
+    public function testCustomerCannotPurchaseWhenNoProductsAdded(): void
+    {
+        $this->expectException(NoProductAddedToPurchase::class);
+
+        $customer = new Customer(uniqid('c', true), 21);
+
+        $product = new Product(uniqid('p', true), Money::PLN(random_int(1, 500)));
+        $product->purchase();
+
+        $purchaseOrder = new Purchase(uniqid('pr', true), $customer);
+
+        $purchaseOrder->confirm();
+    }
+}
